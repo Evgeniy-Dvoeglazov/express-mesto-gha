c3afb22987ed4fdf1b178a71c9e3e6181d302441
Evgeniy Dvoeglazov <jindv@yandex.ru>
2023-05-21 15:16:27 +0300

    ref 10

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,19 @@
 const jwt = require("jsonwebtoken");
+const UnauthorizedError = require("../errors/not-found-error");
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith("Bearer ")) {
+  const { token } = req.cookies;
+  if (!token) {
     return res.status(401).send({ message: "Необходима авторизация" });
   }
-
-  const token = authorization.replace("Bearer ", "");
   let payload;
 
   try {
     payload = jwt.verify(token, "some-secret-key");
-  } catch (e) {
-    const err = new Error("Необходима авторизация");
-    err.statusCode = 401;
+  } catch (err) {
+    const error = new UnauthorizedError("Необходима авторизация");
 
-    next(err);
+    next(error);
   }
 
   req.user = payload;
